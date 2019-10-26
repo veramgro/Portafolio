c0886506bf4ff06f479895f0112e14d17e788995
veramgro <57042635+veramgro@users.noreply.github.com>
2019-10-26 11:55:33 -0600

    SubirArchivos/Portafolio
    Subi la actividad trabajada en clase.

---
+++ JS/contact.js
@@ -0,0 +1,33 @@
+function sendMensaje(){
+    let nombre, email,mensaje;
+    nombre = document.getElementById('form_name').value;
+    email = document.getElementById('form_email').value;
+    mensaje = document.getElementById('form_message').value;
+
+    Email.send({
+        Host : "smtp.gmail.com", //servidor
+        Username : "veramgro@gmail.com", //Micorrepo
+        Password : "Finn&Jake23", //mic
+        To : 'veramgro@gmail.com', //a donde lo mando
+        From : email, //quien me lo envia
+        Subject : nombre,
+        Body : mensaje
+    }).then(
+      message => alert(message)
+    );
+}
+
+
+
+/*function getEmail(){
+    let email;
+    email = document.getElementById('form_email');
+    alert(email.value);
+}
+function getMensaje(){
+    let mensaje;
+    mensaje = document.getElementById('form_message');
+    alert(mensaje.value);
+}
+alert(nombre+'\n'+email+'\n'+mensaje);
+*/
